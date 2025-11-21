edb8a6e9ece0b2badaedeefb14a0dabc2a68e346
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-11-21 23:38:03 +0100

    autosave

--- src/components/ic/knob.ts
+++ src/components/ic/knob.ts
@@ -1,15 +1,15 @@
 
-export function slider(id: string, name: string): string {
+export function knob(id: string, name: string): string {
 	return `
-<div class="horizontal-slider-container" style="view-transition-name: hsc-${id}; view-transition-class: slider">
-	<div class="slider-track">
-		<div class="slider-fill"></div>
+<div class="rotary-body" style="view-transition-name: kc-${id}; view-transition-class: knob">
+	<div for="${id}" class="rotary-knob">
+		<div class="knob-ring"><span class="knob-indicator"></span></div>
 		<input type="range" id="${id}" name="${name}" min="0" max="500" value="0" step="1">
 	</div>
-	<div class="slider-value">
-		<div class="min">0ms</div><div class="current">0ms</div><div class="max">500ms</div>
-	</div>
 </div>`;
+
+
+
 }
 export function setMaxValue(id: string, max: number, now: number, onInput: (event: Event) => void) {
 	const input = document.querySelector<HTMLInputElement>(`#${id}`);
